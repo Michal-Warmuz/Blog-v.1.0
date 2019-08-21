4a0ac29dd390058ab495d6adbed438aeacbb2478
Michal-Warmuz <warmuz.michal@gmail.com>
2019-08-21 11:52:38 +0200

    Add Ninject

---
+++ Blog.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,30 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Blog.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        // nasz DI container, który wykorzystamy do tworzenia powiązań
+        private IKernel kernel;
+
+        public NinjectControllerFactory()
+        {
+            kernel = new StandardKernel();
+           // kernel.Bind<ICategoryService>().To<CategoryService>();
+
+
+        }
+
+        protected override IController GetControllerInstance(
+            RequestContext reqCtx, Type controllerType)
+        {
+            return kernel.Get(controllerType) as IController;
+        }
+    }
+}
