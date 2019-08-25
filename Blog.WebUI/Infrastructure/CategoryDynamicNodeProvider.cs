73a5139f6b9aaa162cb284020b3cb4aaf3f979cf
Michal-Warmuz <warmuz.michal@gmail.com>
2019-08-25 10:46:02 +0200

    Add SiteMap

---
+++ Blog.WebUI/Infrastructure/CategoryDynamicNodeProvider.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Contracts.Services;
+using Blog.Model;
+using MvcSiteMapProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.WebUI.Infrastructure
+{
+    public class CategoryDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        //private ICategoryService categoryService;
+
+        //public CategoryDynamicNodeProvider(ICategoryService _categoryService)
+        //{
+        //    categoryService = _categoryService;
+        //}
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodes)
+        {
+            var returnValue = new List<DynamicNode>();
+            foreach (var item in db.Categories)
+            {
+                DynamicNode node = new DynamicNode();
+                node.Title = item.Name;
+                node.Key = "Category_" + item.CategoryId;
+                node.RouteValues.Add("categoryId", item.CategoryId);
+                returnValue.Add(node);
+            }
+            return returnValue;
+        }
+    }
+}
